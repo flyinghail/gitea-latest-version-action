1259cf44d6b2b3c4a89eb82a185e6848e1e7f5ca
FlyingHail <flyinghail@msn.com>
2025-02-12 22:27:57 +0800

    action script

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# -gitea-latest-tag-action-Public
+# Gitea Latest Version
+Get the latest version from the tags.
+
+**only use for Gitea action**
+
+## Inputs
+
+### `gitea-token`
+**Required** The Gitea token.
+
+### `prefix`
+Prefix in front of version tag. Default `"v"`.
+Example Prefix `"v"` -> Version `"v0.1.0"`
+
+### `fallback`
+Fallback version if no tags are found. Default `"0.0.0"`.
+
+## Outputs
+
+### `major-version`
+
+The major version. Example: `v1.2.3` -> `1`
+
+### `minor-version`
+
+The minor version. Example: `v1.2.3` -> `1.2`
+
+### `patch-version`
+
+The patch version. Example: `v1.2.3` -> `1.2.3`
+
+### `major-number`
+
+The major number. Example: `v1.2.3` -> `1`
+
+### `minor-number`
+
+The minor number. Example: `v1.2.3` -> `2`
+
+### `patch-number`
+
+The patch number. Example: `v1.2.3` -> `3`
+
+## Example usage
+
+```yml
+- uses: flyinghail/gitea-latest-version-actiong@v1.0.0
+  id: version
+  with:
+      gitea-token: ${{secrets.GITHUB_TOKEN}}
+      fallback: 0.0.0
+```
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as semver from "semver"
+import { giteaApi, type Api } from "gitea-js"
+
+function getGiteaApi() {
+    console.log("Server url", github.context.serverUrl)
+    return giteaApi(github.context.serverUrl, {
+        token: core.getInput('gitea-token'), // generate one at https://gitea.example.com/user/settings/applications
+    })
+}
+
+export async function findLastVersion(api: Api<unknown>, prefix: string) {
+    const {owner, repo} = github.context.repo
+    console.log("Owner", owner)
+    console.log("Repo", repo)
+
+    let page = 1
+
+    while (true) {
+        const {data} = await api.repos.repoListTags(owner, repo, {page, limit: 50})
+        if (data.length === 0) {
+            break
+        }
+
+        for (const k in data) {
+            const tag = data[k]
+            if (tag.name) {
+                const version = getSemverVersion(prefix, tag.name)
+                if (version) {
+                    console.log(`Found tag ${tag.name}`)
+                    return version
+                }
+            }
+        }
+
+        page++
+    }
+
+
+    const fallback = semver.clean(core.getInput('fallback')) || '0.0.0'
+    console.log(`No tags found, using ${fallback}`);
+    return fallback;
+}
+
+function getSemverVersion(prefix: string, tag: string) {
+    if (tag.startsWith(prefix)) {
+        tag = tag.substring(prefix.length)
+        const version = semver.clean(tag)
+        if (version && !version.includes('-')) {
+            return version
+        }
+    }
+
+    return null
+}
+
+async function run() {
+    const prefix = core.getInput('prefix')
+    const giteaApi = getGiteaApi()
+
+    const lastVersion = await findLastVersion(giteaApi, prefix)
+    console.log('Found last version', lastVersion)
+
+    const major = semver.major(lastVersion)
+    const minor = semver.minor(lastVersion)
+    const patch = semver.patch(lastVersion)
+
+    core.setOutput('major-number', major)
+    core.setOutput('minor-number', minor)
+    core.setOutput('patch-number', patch)
+    core.setOutput('major-version', major)
+    core.setOutput('minor-version', `${major}.${minor}`)
+    core.setOutput('patch-version', `${major}.${minor}.${patch}`)
+}
+
+run().catch(e => core.setFailed(e.message));
